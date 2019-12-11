6780c38940834f389701491bc10ba5938947e90f
Håvard Farestveit <hv.farestveit@gmail.com>
2019-12-11 10:01:04 +0800

    created a basic main folder

---
+++ src/main/utils.py
@@ -0,0 +1,4 @@
+
+
+def print_something():
+    print("Hello utils")
---
+++ src/main/vgg-19.py
@@ -0,0 +1,10 @@
+from src.main.utils import print_something
+
+
+class Vgg_19:
+
+    def __init__(self):
+        self.first_string = "hello world"
+
+    def get_from_utils(self):
+        print_something()

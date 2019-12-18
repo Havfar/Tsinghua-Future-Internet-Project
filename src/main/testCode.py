92aa58a4e3255158685f0b58150b74a4db697490
Tobias Skjelvik <trampcha@gmail.com>
2019-12-18 10:34:49 +0800

    testing send recv

--- src/main/testCode.py
+++ src/main/testCode.py
@@ -21,11 +21,32 @@ def init_process(backend='mpi'):
     print("WORLD=", world_size)
     print("RANK=", rank)
 
+def test_send_recv(rank):
+    # dummy tensor
+    d_tensor = torch.zeros(size = 1)
+    req1 = None
+    req2 = None
+    req3 = None
+    if rank == 0:
+        req1 = dist.isend(tensor = d_tensor, dst = 1)
+        req2 = dist.isend(tensor = d_tensor, dst = 2)
+        print("Rank:", rank, "started sending data.")
+        print("Rank:", rank, "data sent:", d_tensor)
+        req1.wait()
+        req2.wait()
+    else:
+        req3 = dist.irecv(tensor = d_tensor, src=0)
+        print("Rank:", rank, "started receiving data.")
+        req3.wait()
+    print("Tensor:", d_tensor, "at rank:", rank)
+
+
 # YOUR TRAINING CODE GOES HERE
 
 
 if __name__ == "__main__":
     init_process()
+    test_send_recv(dist.get_rank())
 
     dist.barrier()
     dist.destroy_process_group()

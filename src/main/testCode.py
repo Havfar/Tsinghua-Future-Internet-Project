2de0d8d32699eb78237aa86020913baf5f7d5984
Tobias Skjelvik <trampcha@gmail.com>
2019-12-18 10:59:10 +0800

    added ceil to bsz

--- src/main/testCode.py
+++ src/main/testCode.py
@@ -2,6 +2,7 @@ import os
 import torch
 import torch.distributed as dist
 import torchvision.datasets as datasets
+from math import ceil
 from torchvision import transforms
 
 
@@ -55,7 +56,7 @@ def partition_dataset():
                                  transforms.Normalize((0.1307,), (0.3081,))
                              ]))
     size = dist.get_world_size()
-    bsz = 128 / float(size)
+    bsz = ceil(128 / float(size))
     partition_sizes = [1.0 / size for _ in range(size)]
     partition = DataPartitioner(dataset, partition_sizes)
     partition = partition.use(dist.get_rank())
